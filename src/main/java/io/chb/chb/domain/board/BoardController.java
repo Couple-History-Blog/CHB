5f5a6168a0fa375977519da56c88e6a3130e5f83
verlnn <mlnsukim2327@gmail.com>
2023-10-10 21:44:24 +0900

    First Commit

--- src/main/java/io/chb/chb/domain/board/BoardController.java
+++ src/main/java/io/chb/chb/domain/board/BoardController.java
@@ -14,6 +14,7 @@ public class BoardController {
     @GetMapping("/test")
     public ResponseEntity<?> testController() {
         log.info("TEST");
+
         return ResponseEntity.ok(true);
     }
 }

607d18885760b8d9f47f8fbb26cdd1d6377c36c1
verlnn <mlnsukim2327@gmail.com>
2023-10-19 21:43:32 +0900

    feat: hikari config

---
+++ src/main/java/io/chb/chb/core/config/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package io.chb.chb.core.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public DataSource serverDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+}

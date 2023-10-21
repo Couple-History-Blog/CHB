0722470b44ceaf4b43e2b94e23d05af75897499f
verlnn <mlnsukim2327@gmail.com>
2023-10-21 20:02:23 +0900

    feat: 사용자 생일, 별명 생성

--- BE/src/main/java/io/chb/chb/domain/user/UserDTO.java
+++ BE/src/main/java/io/chb/chb/domain/user/UserDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -13,14 +14,23 @@ import java.util.Date;
 @AllArgsConstructor
 public class UserDTO {
 
+    private final PasswordEncoder passwordEncoder;
+
+    public UserDTO(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
     // 일반 계정 및 커플 계정 공용
     private String userId;
     private String userPassword;        // 인코딩 된 텍스트
     private String userName;
-    private String sexType;
+    private String userSexType;
+    private String userNickName;
 
     // 일반 계정 전용
     private boolean beCoupleYn;
     private String userRole;
+    private Date userBrthDate;
 
 }

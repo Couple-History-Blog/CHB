95a07d6223bc25589db7862ed8653ab265e1e82e
verlnn <mlnsukim2327@gmail.com>
2023-11-01 06:19:33 +0900

    fix: 회원가입 로딩바 변경

--- FE/web-app/src/utils/loadUtil.tsx
+++ FE/web-app/src/utils/loadUtil.tsx
@@ -1,5 +1,5 @@
 // material-ui
-import { CircularProgress, Dialog, Grid } from '@mui/material';
+import {Backdrop, CircularProgress, Dialog, Grid } from '@mui/material';
 import React from "react";
 import SubCard from "../ui-component/cards/SubCard";
 
@@ -10,13 +10,19 @@ interface LoaderProps {
 
 const Load = ( { isLoading, setIsLoading }: LoaderProps) => {
     return (
-        <Dialog onClose={() => {}} aria-labelledby="simple-dialog-title" open={isLoading}>
+        <Backdrop
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={isLoading}
+        >
+            <CircularProgress color="inherit" />
+        </Backdrop>
+/*        <Dialog onClose={() => {}} aria-labelledby="simple-dialog-title" open={isLoading}>
             {isLoading && (
                 <Grid container justifyContent="center" alignItems="center" sx={{ padding: '2vh 2vw' }}>
                     <CircularProgress color="secondary" aria-label="progress with secondary color" />
                 </Grid>
             )}
-        </Dialog>
+        </Dialog>*/
     )
 }
 

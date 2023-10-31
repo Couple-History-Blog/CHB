270770425f1f4952c0ba6ffbfd050b6100cbeec1
verlnn <mlnsukim2327@gmail.com>
2023-11-01 00:37:49 +0900

    fix: api-url TS로 관리

---
+++ FE/web-app/src/constant/ApiUrls.ts
@@ -0,0 +1,2 @@
+export const SIGN_IN: string = '/sign-in';
+export const CURRENT_USER_INFO: string = '/current-user';
--- FE/web-app/src/constant/api.ts
+++ FE/web-app/src/constant/api.ts
@@ -1,22 +1,11 @@
 import axios from 'utils/axios';
-
-const API_URL = {
-    GET_SCHEDULE_LIST: '/api/schedule/',
-    GET_SCHEDULE_LOG: '/api/schedule/log',
-    GET_SCHEDULE_DETAIL: '/api/schedule/detail',
-    RUN_SCHEDULE_DETAIL: '/api/schedule/detail/run',
-    CANCEL_SCHEDULE_DETAIL: '/api/schedule/detail/cancel',
-    GET_ERROR_LIST: '/api/error/',
-    GET_ERROR_DETAIL_LIST: '/api/error/detail',
-    SIGN_IN: '/sign-in',
-    CURRENT_USER_INFO: '/current-user',
-};
+import { SIGN_IN, CURRENT_USER_INFO } from './ApiUrls';
 
 export const signInAsync = async (params: any) => {
-    return axios.post(API_URL.SIGN_IN, null, { params });
+    return axios.post(SIGN_IN, null, { params });
 }
 
 export const userInfoAsync = async (params: any) => {
     const data = { userJwtToken: params.serviceToken };
-    return axios.get(API_URL.CURRENT_USER_INFO, { params: data });
+    return axios.get(CURRENT_USER_INFO, { params: data });
 }

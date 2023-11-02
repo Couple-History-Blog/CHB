c256044f2eb80eaf1da31e4e2b313552bdbbd38d
verlnn <mlnsukim2327@gmail.com>
2023-11-02 11:25:57 +0900

    feat: 아이디 중복 허용 api async

--- FE/web-app/src/constant/api.ts
+++ FE/web-app/src/constant/api.ts
@@ -1,15 +1,20 @@
 import axios from 'utils/axios';
-import {SIGN_IN, CURRENT_USER_INFO, SIGN_UP} from './ApiUrls';
+import {SIGN_IN, CURRENT_USER_INFO, SIGN_UP, CHECK_AVAILABLE_ID} from './ApiUrls';
 
 export const signInAsync = async (params: any) => {
     return axios.post(SIGN_IN, null, { params });
 }
 
-export const userInfoAsync = async (params: any) => {
-    const data = { userJwtToken: params.serviceToken };
+export const signUpAsync = async (body: any) => {
+    return axios.post(SIGN_UP,  body);
+}
+
+export const userInfoAsync = async (param: any) => {
+    const data = { userJwtToken: param.serviceToken };
     return axios.get(CURRENT_USER_INFO, { params: data });
 }
 
-export const signUpAsync = async (body: any) => {
-    return axios.post(SIGN_UP,  body);
+export const checkAvailableIdAsync = async (param: any) => {
+    const data = { userId: param.id }
+    return axios.get(CHECK_AVAILABLE_ID,  { params:data });
 }

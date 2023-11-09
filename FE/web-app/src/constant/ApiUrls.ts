9aae470b2ae2dbb015de4f2ddbd9df29550e5a4e
verlnn <mlnsukim2327@gmail.com>
2023-11-10 02:15:10 +0900

    feat: 이미지 업로드 api

--- FE/web-app/src/constant/ApiUrls.ts
+++ FE/web-app/src/constant/ApiUrls.ts
@@ -5,3 +5,4 @@ export const CHECK_EXIST_USER: string = '/check-exist-user';
 export const GET_CURRENT_USER_GENDER: string = '/currentUser-gender';
 export const APPLY_COUPLE_ACCOUNT: string = '/apply-couple-account';
 export const GET_USER_PROFILE: string = '/fetch-user-profile';
+export const UPLOAD_PROFILE_IMAGE:string = '/upload-image';

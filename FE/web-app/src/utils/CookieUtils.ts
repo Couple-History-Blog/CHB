17081ae0cdf1bfa9984f097ba012ac6be79b25f2
verlnn <mlnsukim2327@gmail.com>
2023-11-14 22:01:14 +0900

    feat: 쿠키 생성시 한 시간 유효시간

--- FE/web-app/src/utils/CookieUtils.ts
+++ FE/web-app/src/utils/CookieUtils.ts
@@ -1,21 +1,27 @@
 import {KeyedObject} from "../types";
 import jwtDecode from "jwt-decode";
+import {ADMIN, COUPLE, USER} from "../routes/Roles";
 
 export const decodeJwtToken = (serviceToken: string): KeyedObject => {
     return jwtDecode(serviceToken);
 }
 
 export const loadLoginUserInfoData = () => {
+    const USER_ROLE = getCookie("jwt", "roles");
     return {
         userId: getCookie("jwt", "sub"),
         userNickName: getCookie("jwt", "userNickName"),
         userName: getCookie("jwt", "userName"),
+        userRoles: USER_ROLE,
         userEmail: getCookie("jwt", "userEmail"),
         userBirthDate: getCookie("jwt", "userBirthDate"),
         updateProfile: false,
         appliedCoupleAccount: getCookie('jwt', 'appliedCoupleAccount'),
         ownUserAcceptYn: getCookie('jwt', 'ownUserAcceptYn'),
         beCoupleYn: getCookie('jwt', 'beCoupleYn'),
+        isAdmin: USER_ROLE === ADMIN,
+        isCouple: USER_ROLE === COUPLE,
+        isUser: USER_ROLE === USER,
     };
 }
 
@@ -43,7 +49,11 @@ export const getCookie = (cookieName: string, dataName?: string) => {
 }
 
 export const setCookie = (cookieName: string, cookieValue: any) => {
-    document.cookie = `${cookieName}=${cookieValue}; secure`;
+    let date = new Date();
+    const oneHour = 1;
+    date.setTime(date.getTime() + (oneHour * 60 * 60 * 1000));
+    const expires = "; expires=" + date.toUTCString();
+    document.cookie = `${cookieName}=${cookieValue}; secure${expires}`;
 }
 
 export const removeCookie = (cookieName: string) => {

e1472aebf59e0a42719246cf052ca7450a155a3f
verlnn <mlnsukim2327@gmail.com>
2023-11-15 00:12:07 +0900

    fix: log 최소화

--- FE/web-app/src/utils/alertUtil.ts
+++ FE/web-app/src/utils/alertUtil.ts
@@ -1,13 +1,8 @@
 import Swal from "sweetalert2";
-import ko from '../assets/language/ko.json';
 import '../assets/scss/style.scss';
-
-const KOR_LOGIN_MESSAGE = ko['sign-in'];
 
 export const errorSweetAlert = (errMsg: string, alertType: string) => {
     console.log("open error alert!!");
-    console.log("errMsg --> ", errMsg);
-    console.log("alertType --> ", alertType);
     const Toast = Swal.mixin({
         toast: true,
         position: 'top-end',
@@ -33,8 +28,6 @@ export const errorSweetAlert = (errMsg: string, alertType: string) => {
 
 export const successSweetAlert = (sucMsg: string, alertType: string) => {
     console.log("open success alert!!");
-    console.log("sucMsg --> ", sucMsg);
-    console.log("alertType --> ", alertType);
     const Toast = Swal.mixin({
         toast: true,
         position: 'top-end',
@@ -44,7 +37,6 @@ export const successSweetAlert = (sucMsg: string, alertType: string) => {
         didOpen: (toast) => {
             toast.addEventListener('mouseenter', Swal.stopTimer)
             toast.addEventListener('mouseleave', Swal.resumeTimer)
-            // toast.style.zIndex = String(10000); // 원하는 Z-인덱스 값으로 설정
         },
         customClass: {
             container: 'swal-toast-container' // 앞서 정의한 CSS 클래스 이름을 여기에 지정
